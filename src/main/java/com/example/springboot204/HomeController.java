c40993466035cb594048ba24e63ffb5a753a306d
yifty08 <yifty08@icloud.com>
2019-03-12 13:24:09 -0400

    form validation

---
+++ src/main/java/com/example/springboot204/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.springboot204;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @GetMapping("/tvform")
+    public String loadTvForm(Model model) {
+        model.addAttribute("tvshow", new Tvshow());
+        return "tvform";
+    }
+
+    @PostMapping("/tvform")
+    public String processTvForm(@Valid Tvshow tvshow, BindingResult result) {
+//        System.out.println("hello");
+        if (result.hasErrors()) {
+//            System.out.println("error");
+            return "tvform";
+        }
+//        System.out.println("no error");
+//        else {return "tvshowconfirm";}
+        return "tvshowconfirm";
+    }
+}
---
+++ src/main/java/com/example/springboot204/Springboot204Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot204;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot204Application.class, args);
+	}
+
+}
